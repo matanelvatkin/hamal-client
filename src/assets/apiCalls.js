ac66cd3a1c2e04b18c47731e290f3b1bdd384aee
matanelvatkin <matanelvatkin@gmail.com>
2023-10-23 16:06:37 +0300

    for vercel

--- src/assets/apiCalls.js
+++ src/assets/apiCalls.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.SERVER_BASE_URL
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_BASE_URL
 
 const apiCalls = async (method, url, data) => {
     axios.defaults.headers.common.Authorization = `Bearer ${localStorage.token}`
