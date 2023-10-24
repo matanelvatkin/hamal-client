6b48fc5e3798e4d5964f444d43b935eaaf6275ac
matanelvatkin <matanelvatkin@gmail.com>
2023-10-24 18:10:54 +0300

    add loader

--- src/components/AdminComponents/index.jsx
+++ src/components/AdminComponents/index.jsx
@@ -7,68 +7,92 @@ import React, { useContext, useEffect, useRef, useState } from "react";
 
 export default function AdminComponents() {
   const [allUsers, setAllUsers] = useState([]);
-  const [actives, setActives] = useState({actives:0,notActives:0});
-  const [loader,setLoader] = useState({fullName:''})
-  const addUserRef  =useRef()
+  const [actives, setActives] = useState({ actives: 0, notActives: 0 });
+  const [loader, setLoader] = useState({ fullName: "" });
+  const addUserRef = useRef();
   const getUsers = async () => {
     const results = await apiCalls("get", "user/allusers");
-    changeActives(results.data)
+    changeActives(results.data);
     setAllUsers(results.data);
   };
-  const changeActives=(usersArray=[])=>{
-    let actives = 0
+  const changeActives = (usersArray = []) => {
+    let actives = 0;
     console.log(usersArray);
-    let notActives =0
+    let notActives = 0;
     if (usersArray.length > 0) {
-      usersArray.forEach(user =>user.isActive?actives++:notActives++);
-      setActives({actives,notActives})
+      usersArray.forEach((user) => (user.isActive ? actives++ : notActives++));
+      setActives({ actives, notActives });
     }
-  }
+  };
   const updateActive = async (user) => {
-    setLoader(prev=>user)
+    setLoader((prev) => user);
     await apiCalls("put", "user/update", {
       fullName: user.fullName,
     });
     const results = await apiCalls("get", "user/allusers");
-    changeActives(results.data)
-    setAllUsers(prev=>results.data);
+    changeActives(results.data);
+    setAllUsers((prev) => results.data);
   };
   const adminAddUser = async (e) => {
-    e.preventDefault()
-    const results = await apiCalls("post","user/userfromadmin",{fullName:addUserRef.current.value})
+    e.preventDefault();
+    const results = await apiCalls("post", "user/userfromadmin", {
+      fullName: addUserRef.current.value,
+    });
     getUsers();
-    addUserRef.current.value=''
-  }
-  useEffect(()=>{
+    addUserRef.current.value = "";
+  };
+  useEffect(() => {
     console.log(loader);
-    if(loader.fullName){
-      setLoader({fullName:''})
+    if (loader.fullName) {
+      setLoader({ fullName: "" });
     }
-  },[allUsers])
-  useEffect(()=>{
-    getUsers()
-  },[])
+  }, [allUsers]);
+  useEffect(() => {
+    getUsers();
+  }, []);
   return (
     <div className={style.main}>
       <form className={style.form} onSubmit={adminAddUser}>
-      <Input placeholder='שם מלא' inputRef={addUserRef} className={style.input_admin}/>
-      <Button className={style.Button} type='submit' text="הוסף משתמש"/>
+        <Input
+          placeholder="שם מלא"
+          inputRef={addUserRef}
+          className={style.input_admin}
+        />
+        <Button className={style.Button} type="submit" text="הוסף משתמש" />
       </form>
-      <div><span>נוכחים: {actives.actives}</span>  <span> לא נוכחים: {actives.notActives}</span></div>
+      <div>
+        <span>נוכחים: {actives.actives}</span>{" "}
+        <span> לא נוכחים: {actives.notActives}</span>
+      </div>
       {allUsers.length > 0 &&
         allUsers.map((user) => {
-          return <div key={user._id} className={style.userDiv}>
-          {user.fullName}
-          <Button
-          className={style.Button}
-          type="button"
-          text={loader.fullName===user.fullName?'מעדכן':user.isActive ? "נוכח" : "לא נוכח"}
-          style={loader.fullName===user.fullName?{backgroundColor:'green'}:user.isActive?{backgroundColor:'darkgrey'}:{backgroundColor:'red'}}
-          onClick={()=>{updateActive(user)}}
-          />
-          </div>
-        }
-        )}
+          return (
+            <div key={user._id} className={style.userDiv}>
+              {user.fullName}
+              <Button
+                className={style.Button}
+                type="button"
+                text={
+                  loader.fullName === user.fullName
+                    ? "מעדכן"
+                    : user.isActive
+                    ? "נוכח"
+                    : "לא נוכח"
+                }
+                style={
+                  loader.fullName === user.fullName
+                    ? { backgroundColor: "green" }
+                    : user.isActive
+                    ? { backgroundColor: "darkgrey" }
+                    : { backgroundColor: "red" }
+                }
+                onClick={() => {
+                  updateActive(user);
+                }}
+              />
+            </div>
+          );
+        })}
     </div>
   );
 }
--- src/components/UserComponents/index.jsx
+++ src/components/UserComponents/index.jsx
@@ -2,29 +2,43 @@ import React, { useContext, useEffect, useState } from "react";
 import { userContext } from "../../App";
 import Button from "../Button";
 import apiCalls from "../../assets/apiCalls";
-import style from "./style.module.css"
+import style from "./style.module.css";
 
 export default function UserComponents() {
   const { user, setUser } = useContext(userContext);
   const [active, setActive] = useState();
+  const [loader, setLoader] = useState(false);
   const updateActive = async () => {
-    
+    setLoader((prev) => !prev);
     const updateUser = await apiCalls("put", "user/update", {
       fullName: user.fullName,
     });
     setUser(updateUser.data);
   };
-  useEffect(()=>{
+  useEffect(() => {
+    setLoader((prev) => false);
     setActive(user.isActive);
-  },[user])
+  }, [user]);
   return (
     <div className={style.userDiv}>
       {user.fullName}
       <Button
         className={style.Button}
         type="button"
-        text={active ? "נוכח" : "לא נוכח"}
-        style={user.isActive?{backgroundColor:'darkgrey'}:{backgroundColor:'red'}}
+        text={
+          loader
+            ? "מעדכן"
+            : user.isActive
+            ? "נוכח"
+            : "לא נוכח"
+        }
+        style={
+          loader
+            ? { backgroundColor: "green" }
+            : user.isActive
+            ? { backgroundColor: "darkgrey" }
+            : { backgroundColor: "red" }
+        }
         onClick={updateActive}
       />
     </div>
