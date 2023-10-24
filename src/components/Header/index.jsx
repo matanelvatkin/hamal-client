f12f16a41cc8194dcc17a9fbe4134d7f51ecb63b
matanelvatkin <matanelvatkin@gmail.com>
2023-10-24 13:19:27 +0300

    add logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import style from "./style.module.css";
 
+
 export default function Header( ) {
   return (
     <div className={style.main_header}>
-      חמ״ל עפרה
+      <span> חמ״ל עפרה</span>
+     <img src="../../../public/logo.png" alt="" />
     </div>
   );
 }
